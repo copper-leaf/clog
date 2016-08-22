2a2712c9ea0867501fc24ae4e3b8eebf9ce04093
Casey Brooks <cjbrooks12@gmail.com>
2016-08-22 18:58:58 -0500

    Attempts to set up as library

--- app/src/test/java/com/caseyjbrooks/clog/ExampleUnitTest.java
+++ app/src/test/java/com/caseyjbrooks/clog/ExampleUnitTest.java
@@ -13,5 +13,8 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
+        String output = Clog.formatString("{{ $1 }} {{ $2 }}", 1, 2);
+        assertEquals("1 2", output);
     }
 }
